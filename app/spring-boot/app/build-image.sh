eeac107961cee9b5b7a3cfafa532f6dce37f9fd6
Ani K.P <ani.prabhakaran@gmail.com>
2022-05-04 09:46:04 +0000

    added steps to use img tool runtime to build container image

--- app/spring-boot/app/build-image.sh
+++ app/spring-boot/app/build-image.sh
@@ -1,35 +1,32 @@
 #/bin/bash
 
 # login to push image registry
-buildah login -u ${PUSH_IMAGE_REGISTRY_UNAME} -p ${PUSH_IMAGE_REGISTRY_PWD} ${PUSH_IMAGE_REGISTRY}
-
+img login -u ${PUSH_IMAGE_REGISTRY_UNAME} -p ${PUSH_IMAGE_REGISTRY_PWD} ${PUSH_IMAGE_REGISTRY}
+if [ $? -ne 0 ]; then
+   echo "Failed to login to docker registry ${PUSH_IMAGE_REGISTRY}"
+   exit 1
+fi
+echo "logged into the registry ${PUSH_IMAGE_REGISTRY}"
 # check if application img is already available in the registry
-skopeo inspect docker://${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} > inspect.log 2>&1
+img inspect docker://${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} > inspect.log 2>&1
 error_count=$(grep -E "error|Error" -c inspect.log)
 if [ $error_count -gt 0 ]; then
    touch app-deploy-flag.txt
 fi
-
-# login to pull image registry
-buildah login -u ${PULL_IMAGE_REGISTRY_UNAME} -p ${PULL_IMAGE_REGISTRY_PWD} ${PULL_IMAGE_REGISTRY}
+echo "inspection of image completed ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION}"
 
 #build image
-buildah bud --format=docker --isolation=chroot -t ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} .
+img build -t ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} .
 if [ $? -ne 0 ]; then
-   echo "Failed to build application image"
+   echo "Failed to build application image ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION}"
+   exit 1
+fi
+echo "image ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} build completed" 
+#push image
+img push ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION}
+if [ $? -ne 0 ]; then
+   echo "Failed to push application image ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION}"
    exit 1
 fi
 
-# import gpg key to sign & push image
-gpg --import ${PIPELINE_HOME}/.artisan/keys/root_rsa_key.pgp
-
-#sign the application image & push into the image registry
-skopeo copy --sign-by=${CRYPTO_KEY_EMAIL} --dest-tls-verify=false --dest-creds ${PUSH_IMAGE_REGISTRY_UNAME}:${PUSH_IMAGE_REGISTRY_PWD} containers-storage:${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} docker://${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION}
-
-# if the exit code of the previous command is not zero then retry to push image
-if [ "$?" -ne 0 ]; then
-    skopeo copy --sign-by=${CRYPTO_KEY_EMAIL} --dest-tls-verify=false --dest-creds ${PUSH_IMAGE_REGISTRY_UNAME}:${PUSH_IMAGE_REGISTRY_PWD} containers-storage:${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} docker://${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION}
-fi
-
-# delete files
-rm -f app-deploy-flag.txt inspect.log
+echo "image ${PUSH_IMAGE_REGISTRY}/${PUSH_IMAGE_REPO}/${PUSH_IMAGE_NAME}:${PUSH_IMAGE_VERSION} push completed" 
